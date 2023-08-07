22bd2750373062ce856af0f37f1c5eeb32e3d658
uroojisaar <uroojisaar@gmail.com>
2023-08-07 23:30:45 +0500

    Conditional logic front end

--- src/app/widgets-toolbox/widgets-toolbox.component.ts
+++ src/app/widgets-toolbox/widgets-toolbox.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Widget, WidgetConfig } from '../form-builder/model';
+import { FormBuilder, FormGroup } from '@angular/forms';
+
 
 @Component({
   selector: 'app-widgets-toolbox',
@@ -6,6 +9,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./widgets-toolbox.component.scss']
 })
 export class WidgetsToolboxComponent {
+
+  @Input('item') item : Widget | { [key: string]: any; } = {};
+  @Input('items') items : Widget[] | any = [];
+  selectedValue1: string | null = null;
+  selectedValue2: string | null = null;
+  secondSelectOptions: { label: string, value: string; }[] = [];
+  selectedWidget: Widget | { [key: string]: any; } = {};
+
+  constructor(private formBuilder: FormBuilder) {}
+
+  // Function to handle when the first select option changes
+  onFirstSelectChange() {
+    this.updateSecondSelectOptions();
+  }
+
+  // Function to update the options of the second select based on the selected value of the first select
+  updateSecondSelectOptions() {
+
+    this.selectedWidget = this.items.find((widget: any) => {
+      return widget.id === this.selectedValue1;
+    });
+    this.secondSelectOptions = this.selectedWidget.type.templateOptions.options;
+    this.selectedValue2 = null; // Reset the value of the second select when the first select changes
+  }
+  
   onDragStart(event: DragEvent) {
     event.dataTransfer?.setData("text", (event.target as HTMLDivElement).id);
   }
